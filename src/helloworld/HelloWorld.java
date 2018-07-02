1a97d34a5fa8e201d07f539360d58ad64a936279
Lee <Lee@DESKTOP-17PIFJQ>
2018-07-02 13:06:34 +0800

    test

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World! from libin Ï¹jb¸ÄÒ»ÏÂ");
+		System.out.println("Hello World! from libin xiugaiyixia333");
 
 	}
 
